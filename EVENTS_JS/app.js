34e011e9a4374e514b4f9a83da3098ce8e30ba29
commitsByShubham <contact.shubhambisht@gmail.com>
2026-07-05 23:59:00 +0530

    DAILY PROGRESS

---
+++ EVENTS_JS/app.js
@@ -0,0 +1,25 @@
+// let form = document.querySelector("form");
+
+// form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+// });
+
+
+// let user = document.querySelector("#user");
+
+// user.addEventListener("change", () => {
+//     console.log("change event");
+//     console.log("final value = ", user.value);
+// });
+// user.addEventListener("input", () => {
+//     console.log("input event");
+//     console.log("final value = ", user.value);
+// });
+
+let inp = document.querySelector("#text");
+let p = document.querySelector("p");
+
+inp.addEventListener("input", function () {
+    console.log(inp.value);
+    p.innerText = inp.value;
+})
