38a768c1617fd2a3fadaf5ec807fa96f41fb83f1
commitsByShubham <contact.shubhambisht@gmail.com>
2026-07-05 23:41:14 +0530

    DAILY PROGRESS

--- FORM_EVENTS/app.js
+++ FORM_EVENTS/app.js
@@ -1,6 +1,33 @@
-let form = document.querySelector("form");
+// let form = document.querySelector("form");
 
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     alert("Form Submitted");
+// })
+// let form = document.querySelector("form");
+
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     let inp = document.querySelector("input");
+//     console.dir(inp);
+//     console.log(inp.value); //not innerText
+// });
+
+let form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    alert("Form Submitted");
-})
+    console.dir(form);
+    
+    let user = this.elements[0]; //form elements
+    let pass = this.elements[1];
+
+    // let user = document.querySelector("#user");
+    // let pass = document.querySelector("#pass");
+
+
+    console.log(user.value);
+    console.log(pass.value);
+
+    alert(`Hi ${user.value}, your password is set to ${pass.value}`);
+
+});
