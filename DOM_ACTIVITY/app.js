baca47fa5ea7d0b7778b61f8cc8f14d99d161156
commitsByShubham <contact.shubhambisht@gmail.com>
2026-07-07 23:08:28 +0530

    DAILY PROGRESS

---
+++ DOM_ACTIVITY/app.js
@@ -0,0 +1,28 @@
+let inp = document.querySelector("input");
+let ul = document.querySelector("ul");
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function(){
+    // console.log(inp.value);
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+
+
+    let dltBtn = document.createElement("button");
+    dltBtn.innerText = "delete" //adds text in button
+    dltBtn.classList.add("delete"); //add class
+
+    item.appendChild(dltBtn);
+    ul.appendChild(item);
+    inp.value = "";
+});
+
+let dltBtns = document.querySelectorAll(".delete");
+
+for(dltBtn of dltBtns){
+    dltBtn.addEventListener("click", function(){
+        let par = this.parentElement;
+        console.log(par);   
+        par.remove();
+    });
+}
